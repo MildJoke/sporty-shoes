17bfd9ce1a2ee28369d7cbdea0aebc27ad4db1d8
MildJoke <MildJoke@users.noreply.github.com>
2025-06-30 16:51:10 +0530

    Spring Boot project with clean structure

---
+++ src/test/java/com/sportyshoes/sportyapp/SportyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.sportyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
